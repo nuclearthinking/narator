425054e5f18fc494a5fd1304433569724879e659
Vlad Radchenko <vradchenko@callpanda.com>
2023-12-04 02:51:46 +0300

    Improved text preprocessing, new voice-actor source, added vocoder.

--- narator/core/dubbing.py
+++ narator/core/dubbing.py
@@ -5,23 +5,7 @@ from TTS.api import TTS
 from rich.progress import Progress, TextColumn, SpinnerColumn
 
 from narator.storage.base import get_next_chapter, save_dubbed_chapter
-
-
-def split_long_line(character: str, index: int, line: str, _result: list) -> list[str]:
-    if len(line) < 250:
-        return [line]
-    _result = _result if _result else []
-    split_index = line.rfind(' ', 0, 250)
-
-    first_part = line[:split_index]
-    second_part = line[split_index + 1 :]
-    _result.append(first_part)
-
-    if len(second_part) > 250:
-        return split_long_line(character, index, second_part, _result)
-    else:
-        _result.append(second_part)
-        return _result
+from narator.core.text_tools import prepare_sentences
 
 
 def start_voiceover(book_id: int, start: int = 0):
@@ -32,7 +16,11 @@ def start_voiceover(book_id: int, start: int = 0):
     ) as progress:
         progress.add_task(description='[green]Loading model ...', total=None)
         device = 'cuda' if torch.cuda.is_available() else 'cpu'
-        model = TTS(model_name='tts_models/multilingual/multi-dataset/xtts_v2', progress_bar=False, gpu=True).to(device)
+        model = TTS(
+            model_name='tts_models/multilingual/multi-dataset/xtts_v2',
+            vocoder_path='vocoder_models/universal/libri-tts/fullband-melgan',
+            progress_bar=False,
+        ).to(device)
 
     with Progress(
         SpinnerColumn(),
@@ -42,21 +30,11 @@ def start_voiceover(book_id: int, start: int = 0):
         task_id = progress.add_task(description='[green]Processing chapters ...', total=None)
         while chapter := get_next_chapter(book_id=book_id, chapter_from=start):
             progress.update(task_id, description=f'[green]Processing chapter {chapter.chapter_number} ...')
-            sentences = []
-            for line in filter(bool, chapter.text.split('\n')):
-                if len(line) < 250:
-                    sentences.append(line)
-                    continue
-                if '. ' in line:
-                    sentences.extend(line.split('. '))
-                    continue
-                else:
-                    sentences.extend(split_long_line(' ', 250, line, []))
-                    continue
-            text = '\n'.join(sentences)
-
+            text = prepare_sentences(chapter.text)
             with NamedTemporaryFile('w+b', suffix='.wav') as wav:
-                model.tts_to_file(text, language='en', file_path=wav.name, speaker_wav='resources/neil_gaiman.wav')
+                model.tts_to_file(
+                    text, language='en', file_path=wav.name, speed=1.15, speaker_wav='resources/chris_lutkin.wav'
+                )
                 wav.seek(0)
                 save_dubbed_chapter(
                     chapter_id=chapter.id,
---
+++ narator/core/text_tools.py
@@ -0,0 +1,29 @@
+import re
+import textwrap
+
+
+def split_line(line: str, limit=250) -> list[str]:
+    pattern = r'(?<=[.!?])( )(?!\s*$|\"|$|\n)'
+    matches = re.split(pattern, line)
+    stripped_sentences = [e.strip() for e in matches]
+    filtered_sentences = list(filter(bool, stripped_sentences))
+    result = []
+
+    for sentence in filtered_sentences:
+        if len(sentence) < limit:
+            result.append(sentence)
+        else:
+            result.extend(textwrap.wrap(sentence, limit))
+
+    return result
+
+
+def prepare_sentences(input_text: str) -> str:
+    sentences = []
+    for line in filter(bool, input_text.split('\n')):
+        if len(line) < 250:
+            sentences.append(line)
+            continue
+
+        sentences.extend(split_line(line, 250))
+    return '\n'.join(sentences)
